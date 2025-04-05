4979556b6bd46576e79d9fe668315ede2e3724c8
BrandonO31 <brandono9192@gmail.com>
2025-04-05 01:00:17 -0400

    requirements.txt & basic gui

---
+++ gui.py
@@ -0,0 +1,14 @@
+import dearpygui.dearpygui as dpg
+
+dpg.create_context()
+
+with dpg.window(label="Record", width=1000, height=1000):
+    with dpg.group(horizontal=True):
+        startRec = dpg.add_button(label="Start Rec")
+        endRec = dpg.add_button(label="End Rec")
+
+dpg.create_viewport(title="Content Buddy" , width=1000 , height = 200)
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.start_dearpygui()
+dpg.destroy_context()

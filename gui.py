75c6ebed78cdcaec2b197f9643824eaf34994fcf
BrandonO31 <brandono9192@gmail.com>
2025-04-06 00:51:49 -0400

    added obsws dependency, obs.py created and config.toml created

--- gui.py
+++ gui.py
@@ -2,9 +2,15 @@ import dearpygui.dearpygui as dpg
 
 dpg.create_context()
 
+def start_Rec(sender, app_data, user_data):
+    print(f"Sender: {sender}")
+    print(f"App Data: {app_data}")
+    print(f"User Data: {user_data}")
+
+
 with dpg.window(label="Record", width=1000, height=1000):
     with dpg.group(horizontal=True):
-        startRec = dpg.add_button(label="Start Rec")
+        startRec = dpg.add_button(label="Start Rec" , callback=start_Rec, user_data="Fake user data")
         endRec = dpg.add_button(label="End Rec")
 
 dpg.create_viewport(title="Content Buddy" , width=1000 , height = 200)
---
+++ obs.py
@@ -0,0 +1,8 @@
+import obsws_python as obs
+
+
+#using config.toml which provides host & pswd
+cl = obs.ReqClient()
+
+cl.start_record()
+

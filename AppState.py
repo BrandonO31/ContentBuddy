1c8ba55a33bb85e2c9953f906d12822e626c5b3b
BrandonO31 <brandono9192@gmail.com>
2025-06-12 19:15:24 -0400

    added series_id tracking, added method to obtain scenes based on scenario

--- AppState.py
+++ AppState.py
@@ -1,9 +1,10 @@
 from database import *
 
 class AppState:
-    def __init__(self, db_path="database.db", user_id=1):
+    def __init__(self, db_path="database.db", user_id=1, series_id=1): # user_id, series_id = 1 b/c i only have 1 user and series for now
         self.db_path = db_path
         self.user_id = user_id
+        self.series_id = series_id
 
     def get_episode(self):
         conn = connect_db(self.db_path)
@@ -19,3 +20,9 @@ class AppState:
     def get_next_episode_filename(self):
         ep = self.get_episode()
         return f"RoadTo2000Eloep{ep}.mkv"
+    
+    def get_scene_by_scenario(self, key):
+        conn = connect_db(self.db_path)
+        result = get_series_setting(conn, self.series_id, key)
+
+        return result
